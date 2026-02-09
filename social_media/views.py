9226b179d8b94829891a246ecee7ab7a91c2a965
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-02-09 17:59:12 +0100

    refactor: Improve validation

--- social_media/views.py
+++ social_media/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets, generics, status
+from rest_framework.exceptions import ValidationError, NotFound
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
 
 from social_media.models import Profile
-from social_media.serializers import UserSerializer, ProfileSerializer
+from social_media.serializers import (
+    UserSerializer,
+    ProfileSerializer,
+    UserUpdateSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -15,27 +19,35 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_serializer_class(self):
+        if self.request.method in ("PUT", "PATCH"):
+            return UserUpdateSerializer
+        return UserSerializer
+
     def get_object(self):
         return self.request.user
 
 
 class CreateProfileView(generics.CreateAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = (IsAuthenticated,)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
+        qs = Profile.objects.filter(user=self.request.user)
+        if qs.exists():
+            raise ValidationError({"user": "Profile already exists for this user."})
         serializer.save(user=self.request.user)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
 
 
 class ManageProfileView(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
-        return Profile.objects.get(user=self.request.user)
+        try:
+            profile = Profile.objects.get(user=self.request.user)
+        except Profile.DoesNotExist:
+            raise NotFound("The profile wasn't created for this user yet.")
+        return profile
