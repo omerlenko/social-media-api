98d34068cbe30c5bae2ca78d8166dfcc3e3f079e
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-02-20 13:31:24 +0100

    feat: Add Like & Comment models

--- social_media/models.py
+++ social_media/models.py
@@ -121,3 +121,30 @@ def upload_post_media(instance, filename: str) -> str:
 class PostMedia(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="media")
     file = models.ImageField(upload_to=upload_post_media)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes"
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "post"], name="unique_like")
+        ]
+
+    def __str__(self):
+        return f"{self.post.id} liked by {self.user.username}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"comment: {self.text}"
