08eae81842fb155db9bf7b49b174399d544f5d00
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-02-25 17:57:49 +0100

    fix: Fix bug, where users/me/ endpoint was being inaccessible due to routing conflict

--- social_media/urls.py
+++ social_media/urls.py
@@ -18,7 +18,6 @@ router.register("users", UserViewSet)
 router.register("posts", PostViewSet)
 router.register("comments", CommentViewSet)
 urlpatterns = [
-    path("", include(router.urls)),
     path("auth/register/", CreateUserView.as_view(), name="create_user"),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
@@ -26,6 +25,7 @@ urlpatterns = [
     path("users/me/", ManageUserView.as_view(), name="manage_user"),
     path("profiles/create/", CreateProfileView.as_view(), name="create_profile"),
     path("profiles/me/", ManageProfileView.as_view(), name="manage_profile"),
+    path("", include(router.urls)),
 ]
 
 app_name = "social_media"
